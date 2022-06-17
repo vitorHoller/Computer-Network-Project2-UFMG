f453e81ea6ab95fd54da9d063c3d75e19af539ac
vitor <vitor.holler@outlook.pt>
2022-06-17 17:30:02 -0300

    Iniciate Project

---
+++ client.c
@@ -0,0 +1,84 @@
+#include "common.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+void usage(int argc, char **argv)
+{
+	printf("usage: %s <server IP> <server port>\n", argv[0]);
+	printf("example: %s 127.0.0.1 51511\n", argv[0]);
+	exit(EXIT_FAILURE);
+}
+
+
+int main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		usage(argc, argv);
+	}
+
+	struct sockaddr_storage storage;
+	if (0 != addrparse(argv[1], argv[2], &storage))
+	{
+		usage(argc, argv);
+	}
+
+	int s;
+	s = socket(storage.ss_family, SOCK_STREAM, 0);
+	if (s == -1)
+	{
+		logexit("socket");
+	}
+	struct sockaddr *addr = (struct sockaddr *)(&storage);
+	if (0 != connect(s, addr, sizeof(storage)))
+	{
+		logexit("connect");
+	}
+
+	char addrstr[BUFSZ];
+	addrtostr(addr, addrstr, BUFSZ);
+
+	printf("connected to %s\n", addrstr);
+
+	char buf[BUFSZ];
+	char aux[BUFSZ];
+	memset(buf, 0, BUFSZ);
+	memset(aux, 0, BUFSZ);
+	unsigned total = 0;
+	size_t countr = 0; // count bytes received
+	size_t counts = 0; // count bytes sent
+
+	recv(s, buf, BUFSZ, 0);
+	puts(buf);
+
+	while (1)
+	{
+		memset(buf, 0, BUFSZ);
+		memset(aux, 0, BUFSZ);
+	    printf("mensagem> ");
+		fgets(buf, BUFSZ - 1, stdin);
+		counts = send(s, buf, strlen(buf), 0);
+		if (counts != strlen(buf))
+		{
+			logexit("send");
+		}
+		memset(buf, 0, BUFSZ);
+		countr = recv(s, buf, BUFSZ - total, 0);
+		puts(buf);
+		total += countr; // total of bytes received during the communication
+		printf("received %ld bytes and %u bytes in total\n", countr, total);
+		strcpy(aux, buf);
+        strncpy(buf, aux, strlen(aux)-1); //eliminate '/0'
+		kill(buf, s);
+	}
+	close(s);
+
+	exit(EXIT_SUCCESS);
+}
---
+++ server-mt.c
@@ -0,0 +1,139 @@
+#include "common.h"
+#include "f_server.h"
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define BUFSZ 500
+#define MAX 15
+
+int equip_vector[MAX] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int csock_vector[MAX] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+void usage(int argc, char **argv)
+{
+    printf("usage: %s <v4|v6> <server port>\n", argv[0]);
+    printf("example: %s v4 51511\n", argv[0]);
+    exit(EXIT_FAILURE);
+}
+
+struct client_data
+{
+    int csock;
+    struct sockaddr_storage storage;
+};
+
+void *client_thread(void *data)
+{
+    struct client_data *cdata = (struct client_data *)data;
+    struct sockaddr *caddr = (struct sockaddr *)(&cdata->storage);
+
+    char caddrstr[BUFSZ];
+    addrtostr(caddr, caddrstr, BUFSZ);
+    printf("[log] connection from %s\n", caddrstr);
+
+    char buf[BUFSZ];
+    memset(buf, 0, BUFSZ);
+    char aux[BUFSZ];
+    memset(aux, 0, BUFSZ); 
+    int index = 0;
+    if(add_equip(buf, equip_vector, MAX, &index) > 0){
+        add_csock(cdata->csock, csock_vector, MAX, &index);
+        send(cdata->csock, buf, strlen(buf), 0);
+    }
+    else
+    {
+        send(cdata->csock, "limit exceeded", strlen("limit exceeded"), 0);
+    }
+    
+
+
+    while (1)
+    {
+        recv(cdata->csock, buf, BUFSZ - 1, 0);
+        strcpy(aux, buf);
+        memset(buf, 0, BUFSZ);
+        strncpy(buf, aux, strlen(aux)-1);
+        printf("%ld bytes\n", strlen(buf));
+        send(cdata->csock, buf, strlen(buf), 0);
+        memset(buf, 0, BUFSZ);
+        memset(aux, 0, BUFSZ);
+    }
+    close(cdata->csock);
+
+    pthread_exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        usage(argc, argv);
+    }
+
+    struct sockaddr_storage storage;
+    if (0 != server_sockaddr_init(argv[1], argv[2], &storage))
+    {
+        usage(argc, argv);
+    }
+
+    int s;
+    s = socket(storage.ss_family, SOCK_STREAM, 0);
+    if (s == -1)
+    {
+        logexit("socket");
+    }
+
+    int enable = 1;
+    if (0 != setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)))
+    {
+        logexit("setsockopt");
+    }
+
+    struct sockaddr *addr = (struct sockaddr *)(&storage);
+    if (0 != bind(s, addr, sizeof(storage)))
+    {
+        logexit("bind");
+    }
+
+    if (0 != listen(s, 10))
+    {
+        logexit("listen");
+    }
+
+    char addrstr[BUFSZ];
+    addrtostr(addr, addrstr, BUFSZ);
+    printf("bound to %s, waiting connections\n", addrstr);
+
+    while (1)
+    {
+        struct sockaddr_storage cstorage;
+        struct sockaddr *caddr = (struct sockaddr *)(&cstorage);
+        socklen_t caddrlen = sizeof(cstorage);
+
+        int csock = accept(s, caddr, &caddrlen);
+        if (csock == -1)
+        {
+            logexit("accept");
+        }
+
+        struct client_data *cdata = malloc(sizeof(*cdata));
+        if (!cdata)
+        {
+            logexit("malloc");
+        }
+        cdata->csock = csock;
+        memcpy(&(cdata->storage), &cstorage, sizeof(cstorage));
+
+        pthread_t tid;
+        pthread_create(&tid, NULL, client_thread, cdata);
+    }
+
+    exit(EXIT_SUCCESS);
+}
